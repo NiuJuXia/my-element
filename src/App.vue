52d21bd407556bcb9dfc4e26749ec32d21246402
宋宋梦醒 <>
2020-06-23 17:28:14 +0800

    col完成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-    <el-button type="primary" icon="el-icon-edit" @click="buttonClick"></el-button>
-    <mybutton type="primary" >凄凄切切群主要按钮</mybutton>
-    <button id='qwer'  @click="buttonClic">原生</button>
-    <div class='qw qwe'>div</div>
-    <vuetry></vuetry>s
-    <div>change MAC SourceTrees 2</div>
+    el
+    <button @click='qwer'>qwer</button>
   </div>
 </template>
 
 <script>
-import mybutton from './components/button/button'
-import vuetry from './components/try/try'
+
+import axios from 'axios'
 
 export default {
   data () {
@@ -20,22 +16,16 @@ export default {
     }
   },
   methods: {
-    buttonClick (qw) {
-      console.log(qw.button)
-    },
-    buttonClic () {
-      console.log('元素')
+    qwer () {
+      axios.get('http://mockjs').then((res) => {
+        console.log(res)
+      })
     }
-  },
-  mounted () {
-    if (this.obj.name) {
-      console.log('name')
-    }
-    console.log('qwer')
   },
   components: {
-    mybutton,
-    vuetry
+  },
+  mounted () {
+    console.log(12)
   }
 }
 </script>
@@ -53,29 +43,5 @@ export default {
   height: 350px;
   border: 1px orange solid;
 }
-#qwer {
-  display: inline-block;
-   position: relative;
-    pointer-events: none;
 
-    &:before {
-      pointer-events: none;
-      content: '';
-      position: absolute;
-      left: -1px;
-      top: -1px;
-      right: -1px;
-      bottom: -1px;
-      border-radius: inherit;
-      background-color: rgba(255,255,255,.35);
-    }
-}
-.qw {
-  width: 100px;
-  height: 100px;
-  background: red;
-}
-.qwe{
-  background: green;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './mock/index.js'
+import '@/utils/svgConfig'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
